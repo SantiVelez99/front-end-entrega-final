c612bbae08e072a8c39f37092a50b2bbab761b68
Santiago Velez <santileo133@gmail.com>
2024-05-16 13:50:05 -0300

    menu hamburguesa completado

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -3,22 +3,28 @@ import { NavLink } from "react-router-dom"
 export default function Header() {
     const isAdmin = true
     return (
-        <header className="header-container">
+        <header className="main-header">
+            <div className="bg-menu-container">
+            <input type="checkbox" className="check-menu" id="check-menu"/>
+            <label htmlFor="check-menu" className="burguer-menu">
+                <div className="burguer-line"></div>
+            </label>
             <nav className='nav-menu'>
                 <ul className='nav-list'>
-                    <li><NavLink to="/">Principal</NavLink></li>
-                    <li><NavLink to="/register">Registrarse</NavLink></li>
-                    <li><NavLink to="/login">LogIn</NavLink></li>
-                    <li><NavLink to="/contact">Contacto</NavLink></li>
-                    <li><NavLink to="/about-us">Acerca de</NavLink></li>
+                    <li className='nav-item'><NavLink to="/">Principal</NavLink></li>
+                    <li className='nav-item'><NavLink to="/register">Registrarse</NavLink></li>
+                    <li className='nav-item'><NavLink to="/login">LogIn</NavLink></li>
+                    <li className='nav-item'><NavLink to="/contact">Contacto</NavLink></li>
+                    <li className='nav-item'><NavLink to="/about-us">Acerca de</NavLink></li>
                     {isAdmin && (
                         <>
-                            <li><NavLink to="/admin-product">Admin Product</NavLink></li>
-                            <li><NavLink to="/admin-users">Admin Users</NavLink></li>
+                            <li className='nav-item'><NavLink to="/admin-product">Admin Product</NavLink></li>
+                            <li className='nav-item'><NavLink to="/admin-users">Admin Users</NavLink></li>
                         </>
                     )}
                 </ul>
             </nav>
+            </div>
         </header>
     )
 }
